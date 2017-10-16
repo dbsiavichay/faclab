867fddcb8550185ec93f630cd1ac92e6c70a8e45
Denis Siavichay <dbsiavichay@gmail.com>
2017-10-15 21:15:37 -0500

    Config database

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'faclabdb',
+        'USER': 'postgres',
+        'PASSWORD': '081011',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
